10b9a16376ef0e87601e4d9a0ce2fa2650dc2970
yuting09120310 <yuting09120310@gmail.com>
2026-09-06 23:53:23 +0800

    chore: register SQL Server DbContext

--- src/DMMS.Web/Program.cs
+++ src/DMMS.Web/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DmmsDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), sql => sql.EnableRetryOnFailure()));
 
 var app = builder.Build();
 
